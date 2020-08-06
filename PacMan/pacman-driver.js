504ac3d2c190af0933948a1492487bc6e7c3a272
LazarusOfSuburbia <alecwgreene@gmail.com>
2020-08-06 12:17:49 -0400

    Added hrefs to the links in navbar

--- PacMan/pacman-driver.js
+++ PacMan/pacman-driver.js
@@ -1,12 +1,48 @@
-const $canvas = $("#pacman-canvas");
-const ctx = $canvas.getContext("2D");
+/** @type {Element} @description Canvas description */
+const $canvas = $("#pacman-canvas")[0];
+/** @type {CanvasRenderingContext2D} @description Canvas context */
+const ctx = $canvas.getContext("2d");
 
-function initMap(){
+/** Background color */
+const g_color_background = "darkblue";
+/** Wall color */
+const g_color_wall = "blue";
+
+/**
+ * @function initMap
+ * 
+ * @param {CanvasRenderingContext2D} ctx Canvas drawing context
+ * 
+ * @description Draws the base map
+ */
+function initMap(ctx){
+    // Fill in background
+    ctx.fillStyle = g_color_background;
+    ctx.beginPath();
+    ctx.fillRect(0,0,300,300);
+    ctx.stroke();
+
+    // Translate to offset map
+    ctx.translate(5 + 0.5 *g_pacman_grid.cellsize,5 + 0.5 * g_pacman_grid.cellsize);
+
+    // Draw wall border
+    ctx.strokeStyle = g_color_wall;
+    ctx.beginPath();
+    ctx.rect(0.5 * g_pacman_grid.cellsize, 0.5 * g_pacman_grid.cellsize, (g_pacman_grid.width - 2) * g_pacman_grid.cellsize, (g_pacman_grid.height + 2) * g_pacman_grid.cellsize);
+    ctx.lineWidth = g_pacman_grid.cellsize;
+    ctx.stroke();
+
     // For each row
     for(let i = 0; i < g_pacman_grid.height; i++){
         // For each column
         for(let j = 0; j < g_pacman_grid.width; j++){
-            
+            if(g_pacman_grid.map[i][j] === g_pacman_grid.chars.wall){
+                // Fill square
+                ctx.fillStyle = g_color_wall;
+                ctx.beginPath();
+                ctx.fillRect( (j + 1) * g_pacman_grid.cellsize, (i + 1) * g_pacman_grid.cellsize, g_pacman_grid.cellsize, g_pacman_grid.cellsize);
+                ctx.stroke();
+            }
         }
     }
 }
--- Utilities/grid.js
+++ Utilities/grid.js
@@ -1,5 +1,33 @@
+/**
+ * @class
+ * 
+ * @classdesc A grid of characters to represent a game state
+ * 
+ * @property {Array<Array<String>>} map 2D array of game state
+ * 
+ * @property {Number} width Number of columns
+ * 
+ * @property {Number} height Number of rows 
+ * 
+ * @property {Number} cellsize Dimensions in pixels of a grid square
+ */
 class Grid{
-    constructor(a_width, a_height, a_cellsize){
+    /**
+     * @constructor 
+     * 
+     * @constructs Grid
+     * 
+     * @param {Number} a_height
+     * 
+     * @param {Number} a_width
+     * 
+     * @param {Number} a_cellsize
+     * 
+     * @description Constructs a a_height x a_width grid with the specified cellsize 
+     * 
+     * @returns {Grid}
+     */
+    constructor(a_height, a_width, a_cellsize){
         // The map for tracking object placement
         this.map = [];
 
